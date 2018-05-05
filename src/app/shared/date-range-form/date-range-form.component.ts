c3ad7b7e595ff82bcb66c790727f774c32c56cff
jonas <jonas@jdhost.cern.ch>
2018-05-05 03:11:23 +0200

    Add UTC checkbox

--- src/app/shared/date-range-form/date-range-form.component.ts
+++ src/app/shared/date-range-form/date-range-form.component.ts
@@ -10,6 +10,7 @@ export class DateRangeFormComponent implements OnInit {
     @Input('dateBegin') dateBegin = new Date();
     @Input('dateEnd') dateEnd = new Date();
     @Output('query') onQuery = new EventEmitter();
+    utc = false;
 
     public timeRange = [this.dateBegin, this.dateEnd];
 
@@ -24,12 +25,22 @@ export class DateRangeFormComponent implements OnInit {
             'from': this.timeRange[0],
             'to': this.timeRange[1],
             'tsFrom': this.makeUTCTS(this.timeRange[0]),
-            'tsTo': this.makeUTCTS(this.timeRange[1])
+            'tsTo': this.makeUTCTS(this.timeRange[1]),
+            'strFrom': this.makeISOWithoutMilliseconds(this.timeRange[0]),
+            'strTo': this.makeISOWithoutMilliseconds(this.timeRange[1]),
+            'utc': this.utc
         });
     }
 
     makeUTCTS(date: Date) {
         return date.getTime() - date.getTimezoneOffset() * 60000;
+    }
+
+    makeISOWithoutMilliseconds(date: Date) {
+        if (this.utc) {
+            date = new Date(this.makeUTCTS(date));
+        }
+        return date.toISOString().split('.')[0] + 'Z';
     }
 
 }
