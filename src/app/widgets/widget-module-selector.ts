5a15785987a5924ae6ae64541131d529c5e87701
jonas <jonas@jdhost.cern.ch>
2018-06-13 18:50:49 +0200

    Remove test widgets

--- src/app/widgets/widget-module-selector.ts
+++ src/app/widgets/widget-module-selector.ts
@@ -1,6 +1,4 @@
 export const widgetModuleSelector = {
-    'single': 'app/widgets/test-widget/test-widget.module#TestWidgetModule',
-    'per-channel': 'app/widgets/test-widget/test-widget.module#TestWidgetModule',
     'static-label': 'app/widgets/static-label-widget/static-label-widget.module#StaticLabelWidgetModule',
     'label': 'app/widgets/label-widget/label-widget.module#LabelWidgetModule',
     'time-query': 'app/widgets/time-query-widget/time-query-widget.module#TimeQueryWidgetModule',
--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -9,7 +9,6 @@ import { provideRoutes } from '@angular/router';
             { path: '^', loadChildren: 'app/widgets/static-label-widget/static-label-widget.module#StaticLabelWidgetModule'},
             { path: '^', loadChildren: 'app/widgets/time-query-widget/time-query-widget.module#TimeQueryWidgetModule'},
             { path: '^', loadChildren: 'app/widgets/fill-run-ls-query-widget/fill-run-ls-query-widget.module#FillRunLsQueryWidgetModule'},
-            { path: '^', loadChildren: 'app/widgets/test-widget/test-widget.module#TestWidgetModule'},
             { path: '^', loadChildren: 'app/widgets/label-widget/label-widget.module#LabelWidgetModule'},
             { path: '^', loadChildren: 'app/widgets/event-bus-test-widget/event-bus-test-widget.module#EventBusTestWidgetModule'},
             { path: '^', loadChildren: 'app/widgets/numeric-field/numeric-field.module#NumericFieldModule'},
