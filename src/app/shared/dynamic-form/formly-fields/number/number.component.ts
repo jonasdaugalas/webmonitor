5fa60d9fad0bf39ff928694e6d144ead89b0fe48
jonasdaugalas <jonasdaugalas@gmail.com>
2018-10-18 17:49:48 +0200

    Fix multiple modelChange for number input

--- src/app/shared/dynamic-form/formly-fields/number/number.component.ts
+++ src/app/shared/dynamic-form/formly-fields/number/number.component.ts
@@ -1,9 +1,31 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormControl, DefaultValueAccessor } from '@angular/forms';
 import { FieldType } from '@ngx-formly/core';
+
+
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'formly-field-number',
   templateUrl: './number.component.html',
   styleUrls: ['./number.component.css']
 })
-export class NumberComponent extends FieldType {}
+export class NumberComponent extends FieldType implements AfterViewInit {
+
+    protected lastValue;
+
+    ngAfterContentInit() {
+        // bellow overwriting setValue function to avoid
+        // FormControl.valueChanges firing multiple times
+        // see: https://github.com/angular/angular/issues/12540
+        const originalSetValue = this.field.formControl.setValue.bind(this.field.formControl);
+        this.field.formControl.setValue = (value: any, options: any) => {
+            if (value !== this.lastValue) {
+                this.lastValue = value;
+                originalSetValue(value, options);
+            }
+        };
+
+    }
+
+}
